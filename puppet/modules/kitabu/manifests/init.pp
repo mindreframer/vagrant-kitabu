e192ad91c0ddf89a70e3b092bc9ba677b2cfd9c2
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-28 18:53:03 +0200

    fix kitabu dependencies

--- puppet/modules/kitabu/manifests/init.pp
+++ puppet/modules/kitabu/manifests/init.pp
@@ -1,10 +1,12 @@
 class kitabu{
-  class{"kitabu::dependencies":} ->
-  class{"kitabu::packages":}
+  class{"kitabu::dependencies":}
+  -> class{"kitabu::packages":}
 }
 
 class kitabu::dependencies{
   if ! defined(Package['ruby1.9.3'])    { package { 'ruby1.9.3': ensure => installed } }
+  if ! defined(Package['libxml2-dev'])  { package { 'libxml2-dev': ensure => installed } }
+  if ! defined(Package['libxslt1-dev'])  { package { 'libxslt1-dev': ensure => installed } }
 }
 
 class kitabu::packages{
