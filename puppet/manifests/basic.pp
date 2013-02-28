3c7dd92d668ac74d10585fd8748a3db2ea50a7da
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-28 19:01:51 +0200

    remove runlibrarian

--- puppet/manifests/basic.pp
+++ puppet/manifests/basic.pp
@@ -28,12 +28,6 @@ class basic::helpers{
     sudo puppet apply -vv  --modulepath=$puppet_dir/modules/ $puppet_dir/manifests/main.pp\n",
     mode    => 0755
   }
-
-  # script to run librarian-puppet
-  file{"/usr/local/bin/runlibrarian":
-    content => "cd $puppet_dir &&  sudo librarian-puppet update \n",
-    mode    => 0755
-  }
 }
 
 # brings the system up-to-date after importing it with Vagrant
