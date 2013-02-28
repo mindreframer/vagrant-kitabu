a87a7164a053dfd50de16ee010913667a5ac37f4
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-28 18:52:52 +0200

    fix pygments

--- puppet/modules/pygments_rb/manifests/init.pp
+++ puppet/modules/pygments_rb/manifests/init.pp
@@ -1,4 +1,13 @@
 class pygments_rb{
+  class{"pygments_rb::dependencies":}
+  -> class{"pygments_rb::packages":}
+}
+
+class pygments_rb::dependencies{
+  if ! defined(Package['build-essential'])    { package { 'build-essential': ensure => installed } }
+}
+
+class pygments_rb::packages{
   package{"python-pygments": }
   -> package{"pygments.rb": provider => "gem"}
 }
