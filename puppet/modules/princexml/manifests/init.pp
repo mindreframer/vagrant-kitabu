fa6958a0066bbae272cb685cc2c4510181bbce11
Roman Heinrich <roman.heinrich@gmail.com>
2013-02-28 18:58:54 +0200

    fix prince installation

--- puppet/modules/princexml/manifests/init.pp
+++ puppet/modules/princexml/manifests/init.pp
@@ -12,16 +12,17 @@ class princexml::dependencies{
 class princexml::download{
   exec{"princexml::download":
     command => "wget ${princexml::params::url}",
-    cwd     => "/tmp",
-    unless  => "test -e /tmp/${princexml::params::filename}"
+    cwd     => "/var/tmp",
+    unless  => "test -e ${princexml::params::filename}"
   }
 
   -> exec{"princexml::decompress":
-    command => "tar xvfz /tmp/${princexml::params::filename}",
-    unless  => "test -e /tmp/${princexml::params::folder}"
+    command => "tar xvfz ${princexml::params::filename}",
+    cwd     => "/var/tmp",
+    unless  => "test -e ${princexml::params::folder}"
   }
 
-  -> file{"/tmp/${princexml::params::folder}/install.sh":
+  -> file{"/var/tmp/${princexml::params::folder}/install.sh":
     content => template("princexml/install.sh.erb"),
     mode    => 755
   }
@@ -29,7 +30,7 @@ class princexml::download{
 
 class princexml::install{
   exec{"princexml::install":
-    command => "/tmp/$princexml::params::folder/install.sh",
+    command => "/var/tmp/$princexml::params::folder/install.sh",
     unless  => "test -e /usr/local/bin/prince"
   }
 }
